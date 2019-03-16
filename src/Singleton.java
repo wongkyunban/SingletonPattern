8caf479bb5d48d65da2f47d38b4fdc4adf97b203
wongkyunban <wongkyunban@sina.com>
2019-03-16 23:07:30 +0800

    Initial commit

---
+++ src/Singleton.java
@@ -0,0 +1,21 @@
+public class Singleton {
+    private volatile static Singleton instance;
+
+    private Singleton() {
+    }
+
+    public static  Singleton getInstance() {
+        if (instance == null) {
+            synchronized(Singleton.class) {
+             if(instance == null) {
+                 instance = new Singleton();
+             }
+            }
+        }
+        return instance;
+    }
+
+    public void setMachine(){
+        System.out.println("This machine one!");
+    }
+}
